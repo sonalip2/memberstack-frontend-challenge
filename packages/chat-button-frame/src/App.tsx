3bc2701feb022d395e3c07a77f59601977c00bb3
Sonali <pawarsonali039@gmail.com>
2021-09-28 22:07:54 +0530

    change chat-icon from event listener color data

--- packages/chat-button-frame/src/App.tsx
+++ packages/chat-button-frame/src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import logo from './chat.png';
 import './App.css';
 
+const EVENT_ORIGIN = 'http://localhost:3001'
+
 function App() {
+  const [color, setColor] = useState('#e66465')
+
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      if (event.origin !== EVENT_ORIGIN) {
+        return;
+      }
+      setColor(event.data)
+    }, false);
+  })
+
   return (
     <div className="App">
-      <div className="chat-icon">
+      <div className="chat-icon" style={{backgroundColor: color}}>
         <img src={logo} className="chat-icon-image" alt="logo" />
       </div>
     </div>
