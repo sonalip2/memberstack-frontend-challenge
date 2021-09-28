519f84265a564a6f4e5a0a4968dcdc68c54fac36
Sonali <pawarsonali039@gmail.com>
2021-09-28 22:06:52 +0530

    send color data to iframe window

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import ColorPicker from "./components/ColorPicker";
 import ChatBotIFrame from "./components/ChatBotIFrame";
 import './App.css';
 
+const IFRAME_DOMAIN = 'http://localhost:3002'
+
 const App = () => {
+  const [color, setColor] = useState('#e66465')
+
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newColor = e.target.value
+    setColor(newColor)
+    const iframeEl = document.querySelector("iframe.chatbot-iframe") as HTMLIFrameElement
+    const iframeWindow = iframeEl.contentWindow as Window;
+    iframeWindow.postMessage(newColor, IFRAME_DOMAIN)
   };
 
   return (
     <>
       <ColorPicker
-        value="#e66465"
+        value={color}
         onChange={handleColorChange}
       />
       <ChatBotIFrame />
